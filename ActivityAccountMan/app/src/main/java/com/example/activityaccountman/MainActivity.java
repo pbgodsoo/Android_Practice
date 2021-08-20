e1f44356cc248cdb714b3e5d592aa28e866be3d4
BeomSoo Park <linoleca@naver.com>
2021-08-20 22:19:17 +0900

    0820

--- ActivityAccountMan/app/src/main/java/com/example/activityaccountman/MainActivity.java
+++ ActivityAccountMan/app/src/main/java/com/example/activityaccountman/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activityaccountman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView text = (TextView)findViewById(R.id.textView2);
+        text.setMovementMethod(new ScrollingMovementMethod());
     }
+
+
+
 }
